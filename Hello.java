46c58ee754b17a53924206207985a0b48c8ba15d
Ternadriana <terna.adriana@gmail.com>
2024-04-11 23:30:54 +0300

    My first commit

--- Hello.java
+++ Hello.java
@@ -2,10 +2,6 @@ package Initial_Rep_01;
 
 public class Hello {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-
-
     }
 }
